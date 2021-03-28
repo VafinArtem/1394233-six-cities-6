2be36abb35109026de925703dda07adaa56b7315
Vafin Artem <a.vafin85@gmail.com>
2021-03-28 18:42:09 +0500

    добавил роутинг

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import Main from '../main/main';
+import SignInScreen from '../sign-in-screen/sign-in-screen';
+import Favorites from '../favorites/favorites';
+import FullCard from '../full-card/full-card';
 
 const App = ({cardsCount}) => {
   return (
-    <Main
-      cardsCount={cardsCount}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main
+            cardsCount={cardsCount}
+          />
+        </Route>
+        <Route exact path="/login">
+          <SignInScreen />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/:id">
+          <FullCard />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
