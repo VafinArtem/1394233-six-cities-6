d391d8e95ab45a260e2d41cbcec6d358b91aa5d5
Vafin Artem <a.vafin85@gmail.com>
2021-03-28 20:24:09 +0500

    добавил моки

---
+++ src/mocks/offers.js
@@ -0,0 +1,222 @@
+export const offers = [
+  {
+    city: {
+      name: `Paris`,
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13
+      }
+    },
+    previewImage: `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/16.jpg`,
+    images: [
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/7.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/8.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/5.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/9.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/19.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/18.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/10.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/4.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/20.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/6.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/13.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/11.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/2.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/12.jpg`
+    ],
+    title: `The house among olive `,
+    isFavorite: false,
+    isPremium: true,
+    rating: 3,
+    type: `room`,
+    bedrooms: 1,
+    maxAdults: 1,
+    price: 169,
+    goods: [
+      `Breakfast`,
+      `Air conditioning`,
+      `Laptop friendly workspace`,
+      `Washer`
+    ],
+    host: {
+      id: 25,
+      name: `Angelina`,
+      isPro: true,
+      avatarUrl: `img/avatar-angelina.jpg`
+    },
+    description: `Relax, rejuvenate and unplug in this ultimate rustic getaway experience in the country. In our beautiful screened Pondhouse, you can gaze at the stars and listen to the sounds of nature from your cozy warm bed.`,
+    location: {
+      latitude: 48.83861,
+      longitude: 2.350499,
+      zoom: 16
+    },
+    id: 1
+  },
+  {
+    city: {
+      name: `Brussels`,
+      location: {
+        latitude: 50.846557,
+        longitude: 4.351697,
+        zoom: 13
+      }
+    },
+    previewImage: `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/18.jpg`,
+    images: [
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/8.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/20.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/16.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/9.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/10.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/12.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/15.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/19.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/4.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/13.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/18.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/14.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/7.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/2.jpg`
+    ],
+    title: `Waterfront with extraordinary view`,
+    isFavorite: false,
+    isPremium: false,
+    rating: 4.8,
+    type: `apartment`,
+    bedrooms: 5,
+    maxAdults: 6,
+    price: 310,
+    goods: [
+      `Dishwasher`,
+      `Towels`,
+      `Washer`,
+      `Breakfast`,
+      `Baby seat`,
+      `Air conditioning`,
+      `Fridge`,
+      `Laptop friendly workspace`,
+      `Coffee machine`
+    ],
+    host: {
+      id: 25,
+      name: `Angelina`,
+      isPro: true,
+      avatarUrl: `img/avatar-angelina.jpg`
+    },
+    description: `Relax, rejuvenate and unplug in this ultimate rustic getaway experience in the country. In our beautiful screened Pondhouse, you can gaze at the stars and listen to the sounds of nature from your cozy warm bed.`,
+    location: {
+      latitude: 50.844556999999995,
+      longitude: 4.346697,
+      zoom: 16
+    },
+    id: 2
+  },
+  {
+    city: {
+      name: `Cologne`,
+      location: {
+        latitude: 50.938361,
+        longitude: 6.959974,
+        zoom: 13
+      }
+    },
+    previewImage: `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/14.jpg`,
+    images: [
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/18.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/20.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/2.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/17.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/13.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/5.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/8.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/15.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/10.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/11.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/4.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/3.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/7.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/6.jpg`
+    ],
+    title: `Penthouse, 4-5 rooms + 5 balconies`,
+    isFavorite: false,
+    isPremium: true,
+    rating: 4.2,
+    type: `hotel`,
+    bedrooms: 4,
+    maxAdults: 6,
+    price: 182,
+    goods: [
+      `Laptop friendly workspace`,
+      `Washer`,
+      `Breakfast`,
+      `Air conditioning`
+    ],
+    host: {
+      id: 25,
+      name: `Angelina`,
+      isPro: true,
+      avatarUrl: `img/avatar-angelina.jpg`
+    },
+    description: `This is a place for dreamers to reset, reflect, and create. Designed with a 'slow' pace in mind, our hope is that you enjoy every part of your stay; from making local coffee by drip in the morning, choosing the perfect record to put on as the sun sets.`,
+    location: {
+      latitude: 50.950361,
+      longitude: 6.961974,
+      zoom: 16
+    },
+    id: 3
+  },
+  {
+    city: {
+      name: `Brussels`,
+      location: {
+        latitude: 50.846557,
+        longitude: 4.351697,
+        zoom: 13
+      }
+    },
+    previewImage: `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/13.jpg`,
+    images: [
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/20.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/11.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/3.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/1.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/16.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/10.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/15.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/8.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/5.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/12.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/19.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/4.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/9.jpg`,
+      `https://assets.htmlacademy.ru/intensives/javascript-3/hotel/6.jpg`
+    ],
+    title: `The house among olive `,
+    isFavorite: false,
+    isPremium: false,
+    rating: 5,
+    type: `room`,
+    bedrooms: 1,
+    maxAdults: 1,
+    price: 115,
+    goods: [
+      `Washer`,
+      `Laptop friendly workspace`,
+      `Breakfast`
+    ],
+    host: {
+      id: 25,
+      name: `Angelina`,
+      isPro: true,
+      avatarUrl: `img/avatar-angelina.jpg`
+    },
+    description: `Design interior in most sympathetic area! Complitely renovated, well-equipped, cosy studio in idyllic, over 100 years old wooden house. Calm street, fast connection to center and airport.`,
+    location: {
+      latitude: 50.849557,
+      longitude: 4.374696999999999,
+      zoom: 16
+    },
+    id: 4
+  }
+];
---
+++ src/mocks/reviews.js
@@ -0,0 +1,62 @@
+export const reviews = {
+  1: [{
+    id: 1,
+    user: {
+      id: 12,
+      isPro: true,
+      name: `Isaac`,
+      avatarUrl: `https://assets.htmlacademy.ru/intensives/javascript-3/avatar/3.jpg`
+    },
+    rating: 2,
+    comment: `The house is very good, very happy, hygienic and simple living conditions around it are also very good. I hope to have the opportunity to come back. Thank you.`,
+    date: `2021-03-10T08:04:28.646Z`
+  }],
+  2: [{
+    id: 1,
+    user: {
+      id: 16,
+      isPro: true,
+      name: `Mollie`,
+      avatarUrl: `https://assets.htmlacademy.ru/intensives/javascript-3/avatar/7.jpg`
+    },
+    rating: 2,
+    comment: `Home is amazing. It's like staying in a museum. The rooms, furnishings and artworks are incredible. The views of My Vesuvius`,
+    date: `2021-03-08T08:04:28.646Z`
+  }],
+  3: [{
+    id: 1,
+    user: {
+      id: 18,
+      isPro: true,
+      name: `Sophie`,
+      avatarUrl: `https://assets.htmlacademy.ru/intensives/javascript-3/avatar/9.jpg`
+    },
+    rating: 5,
+    comment: `The deluxe room was a quite comfortable one with all the adequate facilities. The only thing that made me feel uncomfortable was the rude behavior of an impolite staff at the reception desk.`,
+    date: `2021-02-23T08:04:28.647Z`
+  },
+  {
+    id: 2,
+    user: {
+      id: 15,
+      isPro: false,
+      name: `Kendall`,
+      avatarUrl: `https://assets.htmlacademy.ru/intensives/javascript-3/avatar/6.jpg`
+    },
+    rating: 3,
+    comment: `The house is very good, very happy, hygienic and simple living conditions around it are also very good. I hope to have the opportunity to come back. Thank you.`,
+    date: `2021-02-23T08:04:28.647Z`
+  }],
+  4: [{
+    id: 1,
+    user: {
+      id: 11,
+      isPro: false,
+      name: `Jack`,
+      avatarUrl: `https://assets.htmlacademy.ru/intensives/javascript-3/avatar/2.jpg`
+    },
+    rating: 3,
+    comment: `Beautiful space, fantastic location and atmosphere, really a wonderful place to spend a few days. Will be back.`,
+    date: `2021-02-10T08:04:28.647Z`
+  }]
+};
