18ae42b27cdeadf54a93d81d7de824210c7cf18f
Vafin Artem <a.vafin85@gmail.com>
2021-04-01 18:34:54 +0500

    добавил компонент SortingForm

--- src/components/cities/cities.jsx
+++ src/components/cities/cities.jsx
@@ -6,6 +6,7 @@ import {getOfferLocations, getOffersWithCity} from '../../store/offers/selectors
 import Map from '../map/map';
 import OffersList from '../offers-list/offers-list';
 import {CITY_LOCATION_PROP, OFFERS_LOCATION_PROP} from '../../utils/validate';
+import SortingForm from '../sorting-form/sorting-form';
 
 const Cities = ({activeCity, offersAmount, cityLocation, points}) => {
   const [activeOffer, setActiveOffer] = useState(null);
@@ -16,21 +17,7 @@ const Cities = ({activeCity, offersAmount, cityLocation, points}) => {
         <section className="cities__places places">
           <h2 className="visually-hidden">Places</h2>
           <b className="places__found">{offersAmount} places to stay in {activeCity}</b>
-          <form className="places__sorting" action="#" method="get">
-            <span className="places__sorting-caption">Sort by</span>
-            <span className="places__sorting-type" tabIndex={0}>
-              Popular
-              <svg className="places__sorting-arrow" width={7} height={4}>
-                <use xlinkHref="#icon-arrow-select" />
-              </svg>
-            </span>
-            <ul className="places__options places__options--custom places__options--opened">
-              <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-              <li className="places__option" tabIndex={0}>Price: low to high</li>
-              <li className="places__option" tabIndex={0}>Price: high to low</li>
-              <li className="places__option" tabIndex={0}>Top rated first</li>
-            </ul>
-          </form>
+          <SortingForm />
           <OffersList
             setActiveOffer={setActiveOffer}
           />
