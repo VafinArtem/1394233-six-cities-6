223bf94865517bd85bd5054b9d0c48b1f391f057
Vafin Artem <a.vafin85@gmail.com>
2021-04-02 16:00:36 +0500

    поправил компонент signInScreen

--- src/components/sign-in-screen/sign-in-screen.jsx
+++ src/components/sign-in-screen/sign-in-screen.jsx
@@ -2,6 +2,7 @@ import React, {useRef} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {login} from '../../store/api-actions';
+import {Header} from '../header/header';
 
 const SignInScreen = ({userLogin}) => {
   const loginRef = useRef();
@@ -17,28 +18,33 @@ const SignInScreen = ({userLogin}) => {
   };
 
   return (
-    <div className="page__login-container container">
-      <section className="login">
-        <h1 className="login__title">Sign in</h1>
-        <form className="login__form form" action="#" method="post" onSubmit={handleSubmit}>
-          <div className="login__input-wrapper form__input-wrapper">
-            <label className="visually-hidden">E-mail</label>
-            <input className="login__input form__input" type="email" name="email" placeholder="Email" required ref={loginRef} />
-          </div>
-          <div className="login__input-wrapper form__input-wrapper">
-            <label className="visually-hidden">Password</label>
-            <input className="login__input form__input" type="password" name="password" placeholder="Password" required ref={passwordRef} />
-          </div>
-          <button className="login__submit form__submit button" type="submit">Sign in</button>
-        </form>
-      </section>
-      <section className="locations locations--login locations--current">
-        <div className="locations__item">
-          <a className="locations__item-link" href="#">
-            <span>Amsterdam</span>
-          </a>
+    <div className="page page--gray page--login">
+      <Header />
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+          <section className="login">
+            <h1 className="login__title">Sign in</h1>
+            <form className="login__form form" action="#" method="post" onSubmit={handleSubmit}>
+              <div className="login__input-wrapper form__input-wrapper">
+                <label className="visually-hidden">E-mail</label>
+                <input className="login__input form__input" type="email" name="email" placeholder="Email" required ref={loginRef} />
+              </div>
+              <div className="login__input-wrapper form__input-wrapper">
+                <label className="visually-hidden">Password</label>
+                <input className="login__input form__input" type="password" name="password" placeholder="Password" required ref={passwordRef} />
+              </div>
+              <button className="login__submit form__submit button" type="submit">Sign in</button>
+            </form>
+          </section>
+          <section className="locations locations--login locations--current">
+            <div className="locations__item">
+              <a className="locations__item-link" href="#">
+                <span>Amsterdam</span>
+              </a>
+            </div>
+          </section>
         </div>
-      </section>
+      </main>
     </div>
   );
 };
