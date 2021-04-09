ce9ae15eda23314cf05c204a1fc0a4bfbed02d88
Vafin Artem <a.vafin85@gmail.com>
2021-04-09 17:03:29 +0500

    рефакторинг джобавления в избранное

--- src/store/offers/offers.js
+++ src/store/offers/offers.js
@@ -11,6 +11,39 @@ const initialState = {
   loadedOffer: null
 };
 
+const changeFavoriteLoadedOffer = (state) => {
+  state.loadedOffer = Object.assign(
+      {},
+      state.loadedOffer,
+      {isFavorite: !state.loadedOffer.isFavorite}
+  );
+};
+
+const addFavoriteOffer = (state, action) => {
+  state.favorites = [
+    ...state.favorites,
+    action.payload
+  ];
+};
+
+const removeFavoriteOffer = (state, action) => {
+  state.favorites = state.favorites.filter((offer) => offer.id !== action.payload);
+};
+
+const changeFavoriteStatusOffer = (state, action, isRemove) => {
+  let currentIndexOffer;
+  if (isRemove) {
+    currentIndexOffer = findOfferIndex(state.offers, action.payload);
+  } else {
+    currentIndexOffer = findOfferIndex(state.offers, action.payload.id);
+  }
+  state.offers[currentIndexOffer] = Object.assign(
+      {},
+      state.offers[currentIndexOffer],
+      {isFavorite: !state.offers[currentIndexOffer].isFavorite}
+  );
+};
+
 const offers = createReducer(initialState, ((builder) => {
   builder.addCase(ActionType.CHANGE_SORT, (state, action) => {
     state.sortType = action.payload;
@@ -20,49 +53,25 @@ const offers = createReducer(initialState, ((builder) => {
   });
   builder.addCase(ActionType.ADD_FAVORITE_OFFER, (state, action) => {
     if (state.loadedOffer !== null && state.loadedOffer.id === action.payload.id && state.offers.length === 0) {
-      state.loadedOffer = Object.assign(
-          {},
-          state.loadedOffer,
-          {isFavorite: !state.loadedOffer.isFavorite}
-      );
-      state.favorites = [
-        ...state.favorites,
-        action.payload
-      ];
+      changeFavoriteLoadedOffer(state);
+      addFavoriteOffer(state, action);
     } else {
-      state.favorites = [
-        ...state.favorites,
-        action.payload
-      ];
+      addFavoriteOffer(state, action);
       if (state.offers.length > 0) {
-        const currentIndexOffer = findOfferIndex(state.offers, action.payload.id);
-        state.offers[currentIndexOffer] = Object.assign(
-            {},
-            state.offers[currentIndexOffer],
-            {isFavorite: !state.offers[currentIndexOffer].isFavorite}
-        );
+        changeFavoriteStatusOffer(state, action);
       }
     }
   });
   builder.addCase(ActionType.REMOVE_FAVORITE_OFFER, (state, action) => {
     if (state.loadedOffer !== null && state.loadedOffer.id === action.payload && state.offers.length === 0) {
-      state.loadedOffer = Object.assign(
-          {},
-          state.loadedOffer,
-          {isFavorite: !state.loadedOffer.isFavorite}
-      );
+      changeFavoriteLoadedOffer(state);
       if (state.favorites.length > 0) {
-        state.favorites = state.favorites.filter((offer) => offer.id !== action.payload);
+        removeFavoriteOffer(state, action);
       }
     } else {
-      state.favorites = state.favorites.filter((offer) => offer.id !== action.payload);
+      removeFavoriteOffer(state, action);
       if (state.offers.length > 0) {
-        const currentIndexOffer = findOfferIndex(state.offers, action.payload);
-        state.offers[currentIndexOffer] = Object.assign(
-            {},
-            state.offers[currentIndexOffer],
-            {isFavorite: !state.offers[currentIndexOffer].isFavorite}
-        );
+        changeFavoriteStatusOffer(state, action, true);
       }
     }
   });
